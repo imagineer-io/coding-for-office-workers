27b4d379c108dc4686c86a0d989309c36a1b34d5
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-17 17:33:02 +0800

    Add 02-data-type-1.py
    - Learn int, float, str

---
+++ 02-data-type-1.py
@@ -0,0 +1,22 @@
+# 정수 int - integer
+print(1 + 1)
+# 소수 float
+print(1.5 + 2.1)
+# 문자 str - string
+print("hello")
+
+# 문자열 더하기
+print("hello" + "1")
+
+# 정수 소수 간 변경
+print(4 // 2)
+print(int(4 / 2))
+print(float(2))
+
+# 문자열 숫자 간 변경
+print("1")
+print(int("1"))
+print(int("1.5"))
+# 1) 1
+# print(int(1.5))
+# 2) error
