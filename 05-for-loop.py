0e647f490e2d2f93178902ea7cf211753113fbe4
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-18 14:40:20 +0800

    Add 05-for-loop.py
    - Learn for with range function
    - Learn while and infinite loop

---
+++ 05-for-loop.py
@@ -0,0 +1,19 @@
+# for, range
+# while
+
+# # for
+for num in range(1, 10): # cf. [1, 2, 3][0:2]
+    print(num)
+
+num_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+for num in num_list:
+    print(num)
+
+# while
+# while True: # 무한루프
+#     print(1)
+
+a = 1
+while a < 10:
+    print(a)
+    a = a + 1
