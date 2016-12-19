59c1b4de6818ee4ad9b1107da359a872e1ba589f
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-19 13:50:04 +0800

    Add from-1-to-9 limitation to multiplication table

--- week-01-python/06-multiplication-table.py
+++ week-01-python/06-multiplication-table.py
@@ -2,5 +2,13 @@
 # 2) 해당 단을 곱하기 1에서 곱하기 9까지 실행할 것
 
 dan = int(input("몇 단을 출력 하시겠습니까?"))
+# 원래 답안
 for num in range(1, 10):
     print("{} * {} = {}".format(dan, num, dan * num))
+
+# 원래 1~9단까지만 실행하고 싶다면?
+# if dan > 0 and dan < 10:
+#     for num in range(1, 10):
+#         print("{} * {} = {}".format(dan, num, dan * num))
+#     else:
+#         print("1에서 9사이의 숫자를 넣어주세요.")
