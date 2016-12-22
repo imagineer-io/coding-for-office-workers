cdf72b5a2742ba85678ae4dcfa07fc1b7d16cfe8
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-22 16:32:19 +0800

    Add Article and Comment in models.py

--- week-02-django/src/blog/models.py
+++ week-02-django/src/blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=30)
+    contents = models.TextField()
+    view_count = models.IntegerField()
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article)
+    comment = models.CharField(max_length=100)
