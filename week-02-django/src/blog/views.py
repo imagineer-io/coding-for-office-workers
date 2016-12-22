130728518088c543b94b5086d62d4b94ac49ea92
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-22 14:56:01 +0800

    Add base.css into static folder
    - set staticfiles_dirs in settings.py

--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -7,5 +7,5 @@ def index(request):
     # random_number = randint(1, 10)
     # return HttpResponse("Hello, world. {}".format(random_number))
     # return HttpResponse("Hello, world. You're at the index.")
-    name = "irene"
+    name = "marco"
     return render(request, "index.html", { "name" : name })
