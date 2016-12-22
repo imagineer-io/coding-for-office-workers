012d92fe55f15b2d84fa9dfd2c9c1540b98b1072
Minsoo Kwak <marcosdeseul@gmail.com>
2016-12-22 10:27:35 +0800

    Add templates
    - added template folder location in settings.py

--- week-02-django/src/blog/views.py
+++ week-02-django/src/blog/views.py
@@ -1,6 +1,11 @@
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from django.shortcuts import render
+# from random import randint
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+    # random_number = randint(1, 10)
+    # return HttpResponse("Hello, world. {}".format(random_number))
+    # return HttpResponse("Hello, world. You're at the index.")
+    name = "irene"
+    return render(request, "index.html", { "name" : name })
